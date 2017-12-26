3a2210156d3e5fe562e712d307d80824adf384c1
Fran Tello <jfrantello@gmail.com>
2017-12-26 10:20:49 +0100

    Added router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import angular from 'angular'
+import angular from "angular";
+import "@uirouter/angularjs";
+import IndexCtrl from "./IndexCtrl.js";
 
-alert("webpackngular");
+const app = angular.module("app", ["ui.router"])
+
+app.config(function ($stateProvider) {
+  const state = {
+    name: "state",
+    url: "/state",
+    template: "<h1>hello State!</h1>"
+  }
+
+  $stateProvider.state(state);
+});
+
+app.controller("IndexCtrl", IndexCtrl);
